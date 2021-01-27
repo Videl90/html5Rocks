fe4a579c01992f2f59b287d21c19fc14c159d7f3
Videl90 <lorena.sa.0117@gmail.com>
2021-01-26 20:32:50 -0600

    adding items to the list working

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,24 @@
+$(document).ready(function(){
 
+    function renderTodos(){
+        
+    var allToDos =[];
+    var todoIndex = localStorage.length;
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('check');
-  }
-}, false);
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-    var li = document.createElement("li");
-   
-    var inputValue = document.getElementById("todoInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("Debes agregar una tarea :)");
-    } else {
-      document.getElementById("todoList").appendChild(li);
+    $("button").on("click", function(e){
+        e.preventDefault();
+        var todos = $(".todoInput").val();
+        allToDos.push(todos);
+        localStorage.setItem(todoIndex, todos);
+        console.log(todos);
+        var todoText = $("<li>").html(todos);
+        $(".list-group").append(todoText);
+        $(".list-group-item").text(todos);
+    })
     }
-    document.getElementById("todoInput").value = "";
-  
-    
-  } 
+
+    renderTodos();
+})
 
 
 
