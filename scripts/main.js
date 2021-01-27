a2319bba6ad4e0d158716df19e906e79e5ed1013
Videl90 <lorena.sa.0117@gmail.com>
2021-01-26 22:33:20 -0600

    local storage working

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
 
     function renderTodos(){
-        
+    
+    //Array for todos//
     var allToDos =[];
     var todoIndex = localStorage.length;
 
+    //On click event for adding items to the list//
     $("button").on("click", function(e){
         e.preventDefault();
         var todos = $(".todoInput").val();
         allToDos.push(todos);
+        //Local Storage//
         localStorage.setItem(todoIndex, todos);
         console.log(todos);
         var todoText = $("<li>").html(todos);
@@ -17,6 +20,7 @@ $(document).ready(function(){
         $(".list-group-item").text(todos);
     })
 
+    //On click event for stricking out the text//
     $(document).on("click", function(e){
         $target = $(e.target);
             $target.addClass('check')
